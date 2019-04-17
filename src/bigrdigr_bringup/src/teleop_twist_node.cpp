a5afc537171ba001d116ac17dd6872e281f950da
Michael Lucke <lucke096@umn.edu>
2019-04-17 06:24:24 -0500

    oops, now it compiles

--- src/bigrdigr_bringup/src/teleop_twist_node.cpp
+++ src/bigrdigr_bringup/src/teleop_twist_node.cpp
@@ -11,29 +11,27 @@
 
 // ROS Libs
 #include <ros/ros.h>
-#include <sensor_msgs/Joy>
-#include <geometry_msgs/Twist>
+#include <sensor_msgs/Joy.h>
+#include <geometry_msgs/Twist.h>
 
 // Native_Libs
 #include <string>
 
 // Subscribers (inputs)
-//    sub_name1 (sub_name1_type): sub_name1_TOPIC_VALUE
-//      sub_name1_desc
 //    joy_sub (sensor_msgs/Joy): joy
 //      Joystick messages to be translated to velocity commands.
 // Publishers (outputs)
-//    pub_name1 (pub_name1_type): pub_name1_TOPIC_VALUE
-//      pub_name1_desc
 //    cmd_vel_pub (geometry_msgs/Twist): cmd_vel
 // Parameters (settings)
 //    param_name1 (param_name1_type): default=param_name1_default(,param_name1_path)
+//    linear_scale (double): default = 1.0
+//      Insert desc
 
 
 // ROS Node and Publishers
 ros::NodeHandle * nh;
+ros::NodeHandle * pnh;
 ros::Publisher cmd_vel_pub;
-
 
 // ROS Topics
 std::string joy_topic = "joy";
@@ -46,12 +44,14 @@ void joy_callback(const sensor_msgs::Joy::ConstPtr& msg);
 // // For normal types: string, int, float, bool
 //param_name1_type param_name1 = param_name1_default;
 //int enable_button = 0 ;
-
-// Global_vars
-int linear_axis = 1;
-int angular_axis = 2;
 double linear_scale = 1.0;
 double angular_scale = 1.0;
+
+// Global_vars
+const int linear_axis = 1;
+const int angular_axis = 2;
+//double linear_scale = 1.0;
+//double angular_scale = 1.0;
 //std::map<std::string, int> axis_linear_map;
 //std::map<std::string, int> axis_angular_map;
 //std::map<std::string, double> scale_linear_map;
@@ -59,22 +59,21 @@ double angular_scale = 1.0;
 
 int main(int argc, char** argv){
   // Init ROS
-  ros::init(argc, argv, FILE_NAME);
-  nh = new ros::NodeHandle("~");
+  ros::init(argc, argv, "teleop_twist_node");
+  nh = new ros::NodeHandle();
+  pnh = new ros::NodeHandle("~");
   
   // Subscribers
-  //ros::Subscriber sub_name1_sub = nh->subscribe(sub_name1_topic, sub_name1_BUFLEN, sub_name1_callback);
-  ros::subscriber joy_sub = nh->subscribe(joy_topic, 1, joy_callback);
+  ros::Subscriber joy_sub = nh->subscribe(joy_topic, 1, joy_callback);
   
   // Publishers
-  //pub_name1_pub = nh->advertise<pub_name1_typeLHS::pub_name1_typeRHS>(pub_name1_topic, pub_name1_BUFLEN);
   cmd_vel_pub = nh->advertise<geometry_msgs::Twist>(cmd_vel_topic, 1);
   
   // Params
   //nh->param<param_name1_type>(param_name1_path, param_name1, param_name1_default;
-  //nh->param<int>( , linear_axis, 1);
-  //nh->param<int>( , angular_axis, 2);
-  
+  pnh->getParam("linear_scale", linear_scale);
+  pnh->getParam("angular_scale", angular_scale); 
+  std::cout << "Yo, scales: " << linear_scale << std::endl;
   // Spin
   ros::spin();
 }
@@ -82,12 +81,12 @@ int main(int argc, char** argv){
 void joy_callback(const sensor_msgs::Joy::ConstPtr& msg){
   geometry_msgs::Twist cmd_vel_msg;
   
-  cmd_vel_msg.linear.x = msg.axes[1] // This gets changed, forward vector
+  cmd_vel_msg.linear.x = (msg->axes[1])*linear_scale; // This gets changed, forward vector
   cmd_vel_msg.linear.y = 0;
   cmd_vel_msg.linear.z = 0;
   cmd_vel_msg.angular.x = 0;
   cmd_vel_msg.angular.y = 0;
-  cmd_vel_msg.angular.z = msg.axes[2]; // This spin
+  cmd_vel_msg.angular.z = (msg->axes[2])*angular_scale; // This spin
   cmd_vel_pub.publish(cmd_vel_msg);
 }
 /*
@@ -96,3 +95,4 @@ void sub_name2_callback(const sub_name2_typeLHS::sub_name2_typeRHS::ConstPtr& ms
 
 //:END
 }
+*/
