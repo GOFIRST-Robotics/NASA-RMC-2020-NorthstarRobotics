ab17962a3f03b0eb5750f511148847d061b2b55c
Matt <tlach007@umn.edu>
2019-05-07 16:26:57 -0500

    common: updating script to resize windows and use app mode

--- src/bigrdigr_common/scripts/video_server.sh
+++ src/bigrdigr_common/scripts/video_server.sh
@@ -1,7 +1,7 @@
 #rosrun web_video_server web_video_server
 
-chromium-browser --app --new-window  http://192.168.1.19:8080/stream?topic=/usb_cam_pseye0/image_raw &
-chromium-browser --app --new-window  http://192.168.1.19:8080/stream?topic=/usb_cam_pseye1/image_raw &
-chromium-browser --app --new-window  http://192.168.1.19:8080/stream?topic=/usb_cam_pseye2/image_raw &
-chromium-browser --app --new-window  http://192.168.1.19:8080/stream?topic=/usb_cam_pseye3/image_raw &
-chromium-browser --app --new-window  http://192.168.1.19:8080/stream?topic=/usb_cam_pseye4/image_raw &
+chromium-browser --new-window --app=http://192.168.1.19:8080/stream?topic=/usb_cam_pseye0/image_raw --window-position=0,0 --window-size=640,480 &
+chromium-browser --new-window --app=http://192.168.1.19:8080/stream?topic=/usb_cam_pseye1/image_raw --window-position=1280,0 --window-size=640,480 &
+chromium-browser --new-window --app=http://192.168.1.19:8080/stream?topic=/usb_cam_pseye2/image_raw --window-position=0,540 --window-size=640,480 &
+chromium-browser --new-window --app=http://192.168.1.19:8080/stream?topic=/usb_cam_pseye3/image_raw --window-position=1280,540 --window-size=640,480 &
+chromium-browser --new-window --app=http://192.168.1.19:8080/stream?topic=/usb_cam_pseye4/image_raw --window-position=640,0 --window-size=640,480 &
