c0219f960004d001b12cf46dbd64b911f56f0654
Nick Schatz <schat127@umn.edu>
2019-10-03 20:35:47 -0500

    Pull out requirements into Docker
    See https://github.com/nickschatz/rmc-docker for base environment build

--- Ros_Melodic_ReqPkgs.sh
+++ Ros_Melodic_ReqPkgs.sh
@@ -5,23 +5,23 @@ set -x
 
 # Fix Gazebo models
 
-sudo apt-get install -y ros-melodic-can-msgs 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-controller-manager 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-cv-bridge 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-effort-controllers 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-gazebo-msgs 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-gazebo-plugins 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-gazebo-ros 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-gazebo-ros-control 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-joint-state-controller 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-joy 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-move-base 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-nav-core 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-robot-localization 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-robot-state-publisher 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-rviz-imu-plugin 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-socketcan-bridge 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-socketcan-interface 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-tf2 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-tf2-ros 2>&1 >/dev/null ||:
-sudo apt-get install -y ros-melodic-velocity-controllers 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-can-msgs 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-controller-manager 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-cv-bridge 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-effort-controllers 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-gazebo-msgs 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-gazebo-plugins 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-gazebo-ros 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-gazebo-ros-control 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-joint-state-controller 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-joy 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-move-base 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-nav-core 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-robot-localization 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-robot-state-publisher 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-rviz-imu-plugin 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-socketcan-bridge 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-socketcan-interface 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-tf2 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-tf2-ros 2>&1 >/dev/null ||:
+apt-get install -y ros-melodic-velocity-controllers 2>&1 >/dev/null ||:
