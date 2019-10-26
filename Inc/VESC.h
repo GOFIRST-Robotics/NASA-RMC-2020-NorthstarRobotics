10cf2459ba1fe33486e890801122c7dec485d041
Nick Schatz <schat127@umn.edu>
2019-10-26 15:14:41 -0500

    Implement VESC CAN protocol

---
+++ Inc/VESC.h
@@ -0,0 +1,81 @@
+//
+// Created by nick on 10/21/19.
+//
+
+#ifndef NASA_RMC_RT_VESC_H
+#define NASA_RMC_RT_VESC_H
+
+#include "stdint.h"
+#include "can_manager.hpp"
+
+#define VESC_PACKET_SET_CURRENT 1
+#define VESC_PACKET_SET_RPM 3
+#define VESC_PACKET_SET_POS 4 
+#define VESC_PACKET_SET_DUTY 0
+#define VESC_PACKET_STATUS 9 
+#define VESC_PACKET_STATUS_2 14 
+#define VESC_PACKET_STATUS_3 15 
+#define VESC_PACKET_STATUS_4 16
+#define VESC_PACKET_STATUS_5 27
+#define VESC_PACKET_PING 17
+#define VESC_PACKET_PONG 18
+
+typedef struct {
+    uint8_t id;
+    float rpm;
+    float current;
+    float duty;
+    float amp_hours;
+    float amp_hours_charged;
+    float watt_hours;
+    float watt_hours_charged;
+    float temp_fet;
+    float temp_motor;
+    float current_in;
+    float pid_pos_now;
+    int32_t tacho_value;
+    float v_in;
+} VESC;
+
+VESC* create_VESC(uint8_t id);
+void vesc_send_message(VESC* vesc, uint8_t type, uint8_t* message, int length);
+
+void vesc_system_init();
+void handle_vesc_can_recv(rmc_can_msg msg);
+
+/**
+ * Pops a value off the buffer starting at index of the specified length and increments index to match
+ */
+int32_t buffer_pop_int32(uint8_t* buffer, int* index);
+/**
+ * Pops a value off the buffer starting at index of the specified length and increments index to match
+ */
+int16_t buffer_pop_int16(uint8_t* buffer, int* index);
+
+/**
+ * Set duty cycle to specified VESC
+ * @param vesc
+ * @param duty_cycle Duty cycle in ?
+ */
+void vesc_set_duty_cycle(VESC* vesc, float duty_cycle);
+/**
+ * Set velocity to selected VESC
+ * @param vesc
+ * @param rpm Velocity in RPM
+ */
+void vesc_set_rpm(VESC* vesc, float rpm);
+/**
+ * Set current setpoint to specified VESC
+ * @param vesc
+ * @param current Current in Amperes
+ */
+void vesc_set_current(VESC* vesc, float current);
+/**
+ * Set position
+ * @param vesc
+ * @param position Position in ?
+ */
+void vesc_set_position(VESC* vesc, float position);
+
+
+#endif //NASA_RMC_RT_VESC_H
---
+++ Src/VESC.c
@@ -0,0 +1,129 @@
+//
+// Created by nick on 10/21/19.
+//
+
+#include "VESC.h"
+#include "can_manager.hpp"
+#include "stdint.h"
+#include "stdlib.h"
+#include "string.h"
+
+#define VESC_MAP_SIZE 32
+VESC* vesc_map[VESC_MAP_SIZE] = { NULL };
+
+void vesc_system_init() {
+    registerCANMsgHandler(0xFFFFFFFF, handle_vesc_can_recv);
+}
+
+void vesc_send_message(VESC* vesc, uint8_t type, uint8_t *buffer, int length) {
+    enque_can_message(type << 8 | vesc->id, buffer, length);
+}
+
+VESC* create_vesc(uint8_t id) {
+    VESC* vesc = (VESC*) malloc(sizeof(VESC));
+    vesc->id = id;
+    vesc_map[id] = vesc;
+    return vesc;
+}
+
+void delete_vesc(VESC* vesc) {
+    vesc_map[vesc->id] = NULL;
+    free(vesc);
+}
+
+void handle_vesc_can_recv(rmc_can_msg msg) {
+    uint8_t vesc_id = msg.id & 0xFF;
+    VESC* ptr = vesc_map[vesc_id];
+    if (ptr != NULL) {
+        uint8_t cmd_id = msg.id >> 8;
+        int index = 0;
+        uint8_t* buf = msg.buf;
+        switch (cmd_id) {
+            case VESC_PACKET_STATUS: {
+                if (msg.length < 8) {
+                    break;
+                }
+                ptr->rpm = (float) buffer_pop_int32(buf, &index);
+                ptr->current = (float) buffer_pop_int16(buf, &index) / 10.0;
+                ptr->duty = (float) buffer_pop_int16(buf, &index) / 1000.0;
+                break;
+            }
+            case VESC_PACKET_STATUS_2: {
+                if (msg.length < 8) {
+                    break;
+                }
+                ptr->amp_hours = (float) buffer_pop_int32(buf, &index) / 1e4;
+                ptr->amp_hours_charged = (float) buffer_pop_int32(buf, &index) / 1e4;
+                break;
+            }
+            case VESC_PACKET_STATUS_3: {
+                if (msg.length < 8) {
+                    break;
+                }
+                ptr->watt_hours = (float) buffer_pop_int32(buf, &index) / 1e4;
+                ptr->watt_hours_charged = (float) buffer_pop_int32(buf, &index) / 1e4;
+                break;
+            }
+            case VESC_PACKET_STATUS_4: {
+                if (msg.length < 8) {
+                    break;
+                }
+                ptr->temp_fet = (float) buffer_pop_int16(buf, &index) / 10.0;
+                ptr->temp_motor = (float) buffer_pop_int16(buf, &index) / 10.0;
+                ptr->current_in = (float) buffer_pop_int16(buf, &index) / 10.0;
+                ptr->pid_pos_now = (float) buffer_pop_int16(buf, &index) / 50.0;
+                break;
+            }
+            case VESC_PACKET_STATUS_5: {
+                if (msg.length < 6) {
+                    break;
+                }
+                ptr->tacho_value = buffer_pop_int32(buf, &index);
+                ptr->v_in = (float) buffer_pop_int16(buf, &index) / 1e1;
+                break;
+            }
+        }
+    }
+}
+
+void vesc_set_duty_cycle(VESC* vesc, float duty_cycle) {
+    uint8_t buffer[4];
+    int32_t val = (int32_t)(duty_cycle * 100000.0);
+    memcpy(buffer, &val, sizeof(uint8_t)*4);
+    vesc_send_message(vesc, VESC_PACKET_SET_DUTY, buffer, 4);
+}
+
+void vesc_set_rpm(VESC* vesc, float rpm) {
+    uint8_t buffer[4];
+    int32_t val = (int32_t)(rpm);
+    memcpy(buffer, &val, sizeof(uint8_t)*4);
+    vesc_send_message(vesc, VESC_PACKET_SET_RPM, buffer, 4);
+}
+
+void vesc_set_pos(VESC* vesc, float pos) {
+    uint8_t buffer[4];
+    int32_t val = (int32_t)(pos * 1000000.0);
+    memcpy(buffer, &val, sizeof(uint8_t)*4);
+    vesc_send_message(vesc, VESC_PACKET_SET_POS, buffer, 4);
+}
+
+void vesc_set_current(VESC* vesc, float current) {
+    uint8_t buffer[4];
+    int32_t val = (int32_t)(current * 1000.0);
+    memcpy(buffer, &val, sizeof(uint8_t)*4);
+    vesc_send_message(vesc, VESC_PACKET_SET_CURRENT, buffer, 4);
+}
+
+int32_t buffer_pop_int32(uint8_t *buffer, int *index) {
+    int32_t buf;
+    memcpy(&buf, &buffer[*index], sizeof(int32_t));
+    (*index) += sizeof(int32_t);
+    return buf;
+}
+
+int16_t buffer_pop_int16(uint8_t *buffer, int *index) {
+    int16_t buf;
+    memcpy(&buf, &buffer[*index], sizeof(int16_t));
+    (*index) += sizeof(int16_t);
+    return buf;
+}
