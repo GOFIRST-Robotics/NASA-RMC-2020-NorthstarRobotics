dd92be4715957ca1a984ff9db6560c125955ba45
Nick Schatz <schat127@umn.edu>
2019-11-11 17:54:27 -0600

    Reformat can viewer

--- src/rovr_common/scripts/can_viewer.py
+++ src/rovr_common/scripts/can_viewer.py
@@ -15,8 +15,10 @@ MSG_VESC_CMD_RPM = 3
 MSG_VESC_CMD_POS = 4
 MSG_VESC_CMD_CURRENT = 1
 
+
 def pop_int(buf, begin, length):
   return int.from_bytes(data[begin:(begin+length)], byteorder=VESC_BYTE_ORDER, signed=True)
+
 
 def handle_vesc_status(msg_type, data):
   if msg_type == MSG_VESC_STATUS:
@@ -43,6 +45,7 @@ def handle_vesc_status(msg_type, data):
     v_in = pop_int(data, 4, 2) / 10.0
     print(f'Tacho Val: {tacho_val} Voltage In: {v_in}')
 
+
 def handle_vesc_cmd(msg_type, data):
   if msg_type == MSG_VESC_CMD_DUTY:
     duty = pop_int(data, 0, 4) / 100000.0
@@ -52,10 +55,11 @@ def handle_vesc_cmd(msg_type, data):
     print(f"Set RPM: {rpm}")
   elif msg_type == MSG_VESC_CMD_POS:
     pos = pop_int(data, 0, 4) / 1000000.0
-    printf(f"Set Pos Target: {pos}")
+    print(f"Set Pos Target: {pos}")
   elif msg_type == MSG_VESC_CMD_CURRENT:
     current = pop_int(data, 0, 4) / 1000.0
-    printf(f"Set Current Target: {current}")
+    print(f"Set Current Target: {current}")
+
 
 if __name__ == '__main__':
   bus = can.Bus(channel="can0", interface="socketcan")
@@ -67,7 +71,6 @@ if __name__ == '__main__':
     msg_id = msg.arbitration_id
     data = msg.data
     # Split arb id according to arb_id = (message code) << 8 | (source/target device)
-    # This is how the VESC arb ids are made
     source_dev = msg_id & 0xFF
     msg_type = msg_id >> 8
     print(f"{source_dev}\t{msg_type}\t", end="")
@@ -78,8 +81,5 @@ if __name__ == '__main__':
       handle_vesc_cmd(msg_type, data)
     else: # Unknown message, just print data
       for b in data:
-        b_ = f'{b:X}'
-        if len(b_) == 1:
-          b_ = "0"+b_
-        print(b_, end=" ")
+        print(f'{b:02X}', end=" ")
       print()
