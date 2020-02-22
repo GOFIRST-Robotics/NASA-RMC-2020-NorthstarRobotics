adc14917ec8379460b7ca8174d7f9b2bc8c6d8b5
ryan favour <rfavour9@gmail.com>
2020-02-22 14:23:33 -0600

    misc bugfixes to dw_node

--- src/decawave/src/decawave_node.cpp
+++ src/decawave/src/decawave_node.cpp
@@ -124,20 +124,30 @@ void update_callback(const ros::TimerEvent&){
   std::vector<decawave::Anchor> anchors={ anchor_1, anchor_2};
 */
   //check if 2 anchors are present.
-  if(anchors.size()>0){
+  if(anchors.size()>1){
+    ROS_INFO("Anchors seen");
     //compare y vals
     std::vector<double> covariance_vec;
     //if anchor 1 is p1
+    anchors[0].position[0]=0;
+    anchors[0].position[1]=9000;
+    anchors[1].position[0]=0;
+    anchors[1].position[1]=10000;
+
     if (anchors[1].position[1] >anchors[0].position[1]){
       covariance_vec= do_Math( (double)anchors[1].distance/1000 , distance_mesurment_covar , (double)anchors[0].distance/1000 , distance_mesurment_covar,
-      sqrt( pow( (double) anchors[1].position[1] - (double) anchors[0].position[1] , 2) + pow( (double) anchors[1].position[1] - (double) anchors[0].position[1] , 2) )/1000 , base_seperation_covar,
+      sqrt( pow( (double) anchors[1].position[1] - (double) anchors[0].position[1] , 2) + pow( (double) anchors[1].position[0] - (double) anchors[0].position[0] , 2) )/1000 , base_seperation_covar,
       (double) anchors[1].position[0]/1000, (double) anchors[1].position[1]/1000, (double) anchors[0].position[0]/1000, (double) anchors[0].position[1]/1000 );
     }else{//anchor 0 is p1
       covariance_vec= do_Math( (double)anchors[0].distance/1000 , distance_mesurment_covar , (double)anchors[1].distance/1000 , distance_mesurment_covar,
-      sqrt( pow( (double) anchors[1].position[1] - (double) anchors[0].position[1] , 2) + pow( (double) anchors[1].position[1] - (double) anchors[0].position[1] , 2) )/1000 , base_seperation_covar,
-      (double) anchors[0].position[0]/1000,  (double) anchors[0].position[1]/1000, (double) anchors[1].position[0]/1000,  (double)anchors[1].position[1]/1000 );
+      sqrt( pow( (double) anchors[1].position[1] - (double) anchors[0].position[1] , 2) + pow( (double) anchors[1].position[0] - (double) anchors[0].position[0] , 2) )/1000 , base_seperation_covar,
+     (double) anchors[0].position[0]/1000,  (double) anchors[0].position[1]/1000, (double) anchors[1].position[0]/1000,  (double)anchors[1].position[1]/1000 );
     }
 
+    //handle no val
+    if(covariance_vec.size()<36){
+      return;
+    }
     boost::array<double, 36> covariance;
     for(int i = 0; i<36; i++){
       covariance[i]=covariance_vec[i];
@@ -150,7 +160,7 @@ void update_callback(const ros::TimerEvent&){
     pose_msg.header.stamp=now;
     pose_msg.header.seq=seq;
     seq++;
-    pose_msg.header.frame_id="decawave";
+      pose_msg.header.frame_id="decawave";
 
     //take x and y off the back and put them in the pose
     pose_msg.pose.pose.position.y=covariance_vec[37];
@@ -184,6 +194,11 @@ void update_callback(const ros::TimerEvent&){
 //ASSUMES x,y is inside bounds. assumes p1 y is greater than p2 y
 */
 std::vector<double> do_Math(double d0, double d0_err,double d1, double d1_err,double b,double b_err, double p1x, double p1y, double p2x, double p2y){
+  if ( abs(d0-d1) > b){
+      std::cout << "vec: " << d0 << " " << d1 << " " << b << "\n";
+      std::vector<double> bad_val={0};
+      return bad_val;
+    }
   //calc cos theta
   double cos_theta = (pow(b,2) + pow(d0,2) - pow(d1,2) )/(2*b*d0);
   double b_sqr_err= 2*b_err;
@@ -210,6 +225,7 @@ std::vector<double> do_Math(double d0, double d0_err,double d1, double d1_err,do
   //rotate b by theta
   std::vector<double> v2={(p2x-p1x)*cos_theta - (p2y-p1y)*sin_theta , (p2x-p1x)*sin_theta + (p2y-p1y)*cos_theta };
   double v2_len= sqrt(pow(v2[0], 2)+pow(v2[1], 2));
+  std::cout << "vec: " << d0 << " " << d1 << " " << b << "\n";
   v2[0]=v2[0]/v2_len;
   v2[1]=v2[1]/v2_len;
   std::vector<double> v1={v2[1], -v2[0]};
