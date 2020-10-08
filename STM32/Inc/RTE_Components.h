c83af7ab660769304ccd12f2a5ebe5db11b16898
Julia Schatz <schat127@umn.edu>
2020-10-07 19:39:27 -0500

    Remove STM32 code from main repo

--- STM32/Inc/RTE_Components.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by julia on 10/22/19.
-//
-
-#ifndef NASA_RMC_RT_RTE_COMPONENTS_H
-#define NASA_RMC_RT_RTE_COMPONENTS_H
-
-#endif  // NASA_RMC_RT_RTE_COMPONENTS_H
--- STM32/Inc/print.h
+++
@@ -1,7 +0,0 @@
-#ifndef NASA_RMC_RT_PRINT_H
-#define NASA_RMC_RT_PRINT_H
-
-void print(char* string);
-void println(char* string);
-
-#endif  // NASA_RMC_RT_PRINT_H
